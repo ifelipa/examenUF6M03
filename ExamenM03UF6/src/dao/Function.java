fbbe2c972543a5eca64c5e8d41f1411981dea97b
isma <ismael.felipa@gmail.com>
2016-01-14 21:19:53 +0100

    +avances

--- ExamenM03UF6/src/dao/Function.java
+++ ExamenM03UF6/src/dao/Function.java
@@ -1,7 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import db.Conectar;
 
@@ -11,6 +14,33 @@ public class Function {
 	
 	public void createAlgoxStatement(){
 		conexion= Conectar.getInstance().createConnection();
+		String sql = "INSERT INTO feedback.comments" + "(MYUSER, EMAIL, WEBPAGE,DATUM, SUMMARY, COMMENTS) VALUES"
+				+ "(?,?,?,CURRENT_TIMESTAMP,?,?)";
+		try {
+			statement = conexion.createStatement();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
+		
+	}
+	
+	private void writeResultSet(ResultSet resultSet) throws SQLException {
+		
+		while (resultSet.next()) {
+
+			String user = resultSet.getString("myuser");
+			String website = resultSet.getString("webpage");
+			String summary = resultSet.getString("summary");
+			Date date = resultSet.getDate("datum");
+			String comment = resultSet.getString("comments");
+			System.out.println("User: " + user);
+			System.out.println("Website: " + website);
+			System.out.println("Summary: " + summary);
+			System.out.println("Date: " + date);
+			System.out.println("Comment: " + comment);
+			System.out.println("*************************");
+		}
 	}
 }
